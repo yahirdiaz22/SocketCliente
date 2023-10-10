0ac9c72f5ed57dfb21809ad3d867e4ad561307c9
Yahir Diaz <70656659+yahirdiaz22@users.noreply.github.com>
2023-10-09 19:57:59 -0600

    Agregar archivos de proyecto.

---
+++ SocketCliente/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketCliente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string servidorIP = "127.0.0.1"; 
+            int puerto = 11200; 
+
+            Socket clienteSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // Conectar al servidor
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(servidorIP), puerto);
+            clienteSocket.Connect(endPoint);
+
+            // Enviar datos al servidor
+            string mensaje = "¡Hola, servidor!";
+            byte[] mensajeBytes = Encoding.ASCII.GetBytes(mensaje);
+            clienteSocket.Send(mensajeBytes);
+
+            // Recibir respuesta del servidor
+            byte[] buffer = new byte[1024];
+            int bytesRecibidos = clienteSocket.Receive(buffer);
+            string respuesta = Encoding.ASCII.GetString(buffer, 0, bytesRecibidos);
+            Console.WriteLine($"Respuesta del servidor: {respuesta}");
+
+            // Cerrar el socket del cliente
+            clienteSocket.Close();
+            Console.ReadKey();
+        }
+    }
+}
